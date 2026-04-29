c1c6434704c84346e38fe81c2848b25ada9756ea
Омар Аккулов <omarakkulov@gmail.com>
2026-04-29 18:42:16 +0300

    initial commit

---
+++ src/test/java/com/akkulov/reactive_learning/ReactiveLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akkulov.reactive_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
